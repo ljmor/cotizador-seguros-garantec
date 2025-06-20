b5a998bfdcb2a1157b766db94c31d5bc2ca0b9f4
ErickGaona <Erickgaona625@gmail.com>
2025-06-19 22:12:18 -0500

    Formulario registro

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Landing } from './Landing.jsx'
 import  Salud  from './formulario/salud.jsx'
+import Registro from './formulario/registro.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <Landing /> */}
-    <Salud />
+    {/* <Salud /> */}
+    <Registro />
   </StrictMode>,
 )
